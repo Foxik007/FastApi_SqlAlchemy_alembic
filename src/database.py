a91411a57e3c6c6d8e150016240220d331e00dc8
Foxik007 <85826675+Foxik007@users.noreply.github.com>
2023-08-23 11:20:34 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+asyncpg://postgres:7259512aA@localhost:5432/postgres"
+DATABASE_URL = "postgresql+asyncpg://postgres:******pass@localhost:5432/postgres"
 
 
 metadata = MetaData()
